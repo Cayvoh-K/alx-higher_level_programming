37833f93a83ce4446e845680545d554480dc4620
Cayvoh-K <muhiakevin12@gmail.com>
2022-11-01 15:33:03 -0700

    select the best

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,6 @@
+-- lists all records in the second_table with a score >= 10.
+-- Records are ordered by descending score.
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `score` >= 10
+ORDER BY `score` DESC;
