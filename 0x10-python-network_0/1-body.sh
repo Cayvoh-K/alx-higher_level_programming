c666cad37881cc5a0c717c5311d9c89bca1d4ec4
Cayvoh-K <muhiakevin12@gmail.com>
2022-12-17 06:39:31 +0000

    cURL method

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# sends a GET request to the URL,and also displays the body of the response
+curl -sLX GET "$1"
---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# bash script that sends a delete request to the URL and displays the body response
+curl -X DELETE "$1"
