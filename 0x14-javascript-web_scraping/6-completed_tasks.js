cd53337c775e9b6aeee7681719cb66432abb6da6
Cayvoh-K <muhiakevin12@gmail.com>
2023-01-18 14:03:08 +0000

    how many completed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -7,7 +7,8 @@ const resp = {};
 const json = JSON.parse(body);
 for (let i = 0; i < json.length; i++) {
 if (json[i].completed === true) {
-if (rsep[json][i].userId] = 0;
+if (rsep[json][i].userId] === undefined) {
+resp[json[i].userId] = 0;
 }
 resp[json[i].userId]++;
 }
