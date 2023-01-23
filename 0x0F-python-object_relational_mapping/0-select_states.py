3b245251a60001fe4a4767cf857b7bc180d0cd14
Cayvoh-K <muhiakevin12@gmail.com>
2023-01-23 07:46:20 +0000

    ORM

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,9 +6,9 @@ import MySQLdb
 
 
 if __name__ == '__main__':
-    user, password, database = argv[1], argv{2}, argv{3}
+    user, password, database = argv[1], argv[2], argv[3]
     db = MySQLdb.connect(host="localhost", user=user,
-            passwd=password, db=database)
+                         passwd=password, db=database)
     db.query("SELECT * FROM states ORDER BY id")
     r = db.store_result()
     t = r.fetch_row(maxrows=0)
